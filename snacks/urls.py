3afe0ec352cfc58473c61fb8edf8f03bbb3052c9
ibraheem-areeda <ibraheem.areeda@gmail.com>
2023-07-12 23:28:42 +0000

    list view part

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, SnackDetailView, SnackListView
 urlpatterns = [
-    path('',HomeView.as_view(), name='home')
+    path('', SnackListView.as_view(), name='snacks'),
+    path('<pk>',SnackDetailView.as_view(), name="snack_details")
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView ,ListView, DetailView
+from .models import Snack
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+class SnackListView (ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+
+class SnackDetailView (DetailView):
+    template_name = 'Snack_Detail.html'
+    model = Snack
+
+    
